d813556d1e9fbf6c3b0ec555ede23c24c7dc5078
AyumiizZ <korrawitc@gmail.com>
2017-09-29 12:00:23 +0700

    add draw card

--- console_dev/console_black_jack.py
+++ console_dev/console_black_jack.py
@@ -1,2 +1,18 @@
-card = [1]*52
-in
+import random
+card = [True]*52
+def card_info(card_value):
+    card_sym = ['Club','Diamonds','Hearts','Spades']
+    card_type = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+    return (card_value//13)+1, card_sym[card_value%4]+' '+card_type[(card_value//13)+1]
+# 0 = Ace-club 1 = Ace-diamonds ... 50 King-hearts 51 King-spades
+command = input()
+player = []
+# dealer = []
+# draw card
+while command != 'Stand':
+    card_value = random.randint(0,51)
+    while not card[card_value]:
+        card_value = random.randint(0,51)
+    player.append(card_value)
+    card_value,info = card_info(card_value)
+    
