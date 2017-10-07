f3412da4ff61c7c55c939bd502b30cfd09c0a19b
AyumiizZ <korrawitc@gmail.com>
2017-10-08 01:30:09 +0700

    add win/lose text

--- black_jack.py
+++ black_jack.py
@@ -7,9 +7,10 @@ class gameWindow(arcade.Window):
         arcade.set_background_color((119, 221, 119))
         self.card_sprite = [arcade.Sprite('images/card/'+str(i)+'.png') for i in range(52)]
         self.back_card_sprite = arcade.Sprite('images/card/back.png')
-        self.text1 = arcade.create_text("Score: ", arcade.color.BLACK, 12)
-        self.text2 = arcade.create_text("Score: ", arcade.color.BLACK, 12)
+        # self.text1 = arcade.create_text("Score: ", arcade.color.BLACK, 12)
+        # self.text2 = arcade.create_text("Score: ", arcade.color.BLACK, 12)
         self.world = tools.World(width,height)
+        self.busted_show = False
         
     def player_show_card(self):
         c = 100
@@ -30,15 +31,30 @@ class gameWindow(arcade.Window):
             self.card_sprite[i].set_position(c, 450)
             self.card_sprite[i].draw()
     def update_score(self):
-        self.text1 = arcade.create_text("Score: " + str(self.world.human.score), arcade.color.BLACK, 12)
-        self.text2 = arcade.create_text("Score: " + str(self.world.bot.score), arcade.color.BLACK, 12)
-        arcade.render_text(self.text1, 50, 550)
-        arcade.render_text(self.text2, 50, 225)
+        self.human_score_board = arcade.create_text("Score: " + str(self.world.human.score), arcade.color.BLACK, 12)
+        self.bot_score_board = arcade.create_text("Score: " + str(self.world.bot.score), arcade.color.BLACK, 12)
+        arcade.render_text(self.human_score_board, 50, 225)
+        if self.world.reveal:
+            arcade.render_text(self.bot_score_board, 50, 550)
+    def winner(self):
+        self.busted = arcade.create_text("Busted!!!",arcade.color.BLACK,12)
+        self.human_win = arcade.create_text("You win!!!",arcade.color.BLACK,12)
+        self.bot_win = arcade.create_text("You lose!!!",arcade.color.BLACK,12)
+        if self.world.human.busted or self.world.bot.busted:
+            arcade.render_text(self.busted, 250,250)
+        if self.world.human.score > self.world.bot.score and not self.world.human.busted:
+            arcade.render_text(self.human_win, 250,250)
+        if self.world.human.score < self.world.bot.score and not self.world.bot.busted:
+            arcade.render_text(self.bot_win, 250,250)
+
     def on_draw(self):
+        print(self.world.human.card)
+        print(self.world.human.score)
         arcade.start_render()
         self.player_show_card()
         self.bot_show_card()
         self.update_score()
+        self.winner()
         
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
--- tools.py
+++ tools.py
@@ -8,12 +8,30 @@ class Player():
         self.card = []
         self.deck = deck
         self.score = 0
+        self.ACE = 0
+        self.busted = False
     def draw_card(self):
         card_value = random.randint(0,51)
         while not self.deck.card[card_value]:
             card_value = random.randint(0,51)
         self.card.append(card_value)
         self.deck.card[card_value] = False
+        self.add_score(card_value)
+        if self.score > 21:
+            self.busted = True
+        
+    def add_score(self, card_value):
+        if card_value in [0,1,2,3]:
+            self.ACE += 1
+            self.score += 11
+        elif card_value >= 40:
+            self.score += 10
+        else:
+            self.score += card_value//4+1
+        while self.ACE > 0 and self.score > 21:
+            self.ACE -= 1
+            self.score -= 10
+        
 class World:
     def __init__(self, width, height):
         self.width = width
@@ -27,7 +45,7 @@ class World:
         self.human.draw_card() 
         self.bot.draw_card()
     def on_key_press(self, key, key_modifiers):
-        if key == arcade.key.SPACE and not self.reveal:
+        if key == arcade.key.SPACE and not self.reveal and not self.human.busted:
             self.human.draw_card()
         if key == arcade.key.ENTER:
             self.reveal = True
